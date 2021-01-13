e6d78cc487e1c9685a4c6f469cb715d92224841d
mleelive <mleelive@yahoo.co.jp>
2021-01-13 13:50:17 +0000

    [finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,54 +1,76 @@
 class BooksController < ApplicationController
-  def top
+
+    def create
+
+    @book = Book.new(book_params)
+
+    if @book.save
+
+      flash[:success] = 'Book was successfully created.'
+
+      redirect_to book_path(@book.id)
+
+    else
+
+      @books = Book.all
+      render 'index'
+
+    end
+
   end
-    def index
-  @books = Book.all
-  # 以下1行テスト
-  @book=Book.new
-  end
+
+
+def index
+    @books = Book.all
+    @book = Book.new
+end
+
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def create
-    @book = Book.new(book_params)
-    @books = Book.all
-
-    # 以下のif文テスト
-    if @book.save
-    redirect_to("/books/#{@book.id}")
-    flash[:notice] = "Book was successfully created."
-    else
-    render ("books/index")
-    end
+  def new
+    @book = Book.new
   end
-
 
   def edit
     @book = Book.find(params[:id])
   end
 
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-    flash[:update] = "Book was successfully updated."
-    redirect_to("/books/#{@book.id}")
-    else
-    render ("books/edit")
-    end
-  end
-
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to('/books')
-    flash[:success] = "Book was successfully destroyed."
+    flash[:success] = 'Book was successfully destroyed.'
+    redirect_to books_path
   end
+
+  def update
+
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+
+      flash[:success] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+
+    else
+
+      render 'edit'
+    end
+
+  end
+
+
+  def top
+
+  end
+
 
   private
 
-    def book_params
+  def book_params
     params.require(:book).permit(:title, :body)
-    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_01_10_154941) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
